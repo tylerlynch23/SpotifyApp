570bfd998c9db4533c193b3fa1c478fc5a2f5f2d
Tyler Lynch <tjlynch@buffalo.edu>
2018-04-23 18:49:40 -0400

    Add project files.

---
+++ SpotifyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+
+namespace SpotifyApp
+{
+    class Program
+    {
+        public static SpotifyWebAPI spotifyAPI;
+        static void Main(string[] args)
+        {
+            spotifyAPI = new SpotifyWebAPI()
+            {
+                UseAuth = false,
+            };
+            UserAuthentication.Authenticate(spotifyAPI);
+            Console.WriteLine("HEY");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ SpotifyApp/UserAuthentication.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+
+namespace SpotifyApp
+{
+    class UserAuthentication
+    {
+        CommandHandler commandHandler = new CommandHandler();
+        public static async void Authenticate(SpotifyWebAPI spotifyAPI)
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                 "http://localhost:8000",
+                 8000,
+                 "87a1fe44c407454f9b466def737c236a",
+                 Scope.UserReadPrivate,
+                 TimeSpan.FromSeconds(20)
+            );
+
+            try
+            {
+                //This will open the user's browser and returns once
+                //the user is authorized.
+                spotifyAPI = await webApiFactory.GetWebApi();
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            if (spotifyAPI == null)
+                return;
+        }
+        
+
+
+
+    }
+}
